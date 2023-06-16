c13d7072e77bead17b2aa1708520b8afa38633ff
JuanMiGPU <127730874+JuanMiGPU@users.noreply.github.com>
2023-06-16 11:46:00 +0200

    funciona (de verdad) post palabra

--- BD/tablaPalabra.sql
+++
@@ -1,6 +0,0 @@
---tablaPalabra
-create table Palabra(
-    codigo char(2) not null,
-    nombre varchar (20) not null,
-    primary key (codigo)
-);
--- BD/tablaTematica.sql
+++
@@ -1,5 +0,0 @@
-create table Tematica(
-    codigo char(2) not null,
-    nombre varchar (20) not null,
-    primary key (codigo)
-);
